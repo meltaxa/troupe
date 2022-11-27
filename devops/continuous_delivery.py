7fe5b53a4bada3fda6984e9c5b787215feb57ffd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-27 22:40:05 +0000

    Adding tarfile member sanitization to extractall()

--- devops/continuous_delivery.py
+++ devops/continuous_delivery.py
@@ -202,7 +202,26 @@ def catchall(message, text):
     os.chdir(base_dir)
 
     with tarfile.open(base_dir + '/.cache/troupe-new.tar') as tar:
-        tar.extractall(path=base_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=base_dir)
 
     with open(nextlaunch, 'w') as launch_file:
         launch_file.write("Updated Troupe on {device_name}. " +
@@ -305,7 +324,26 @@ def sync(message, url):
             download_file(url, base_dir + '/.cache/' + filename,
                           os.environ['API_TOKEN'])
         with tarfile.open(base_dir + '/.cache/' + filename) as tar:
-            tar.extractall(path=base_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=base_dir)
         with open(last_download, 'w') as last_file:
             print(url, file=last_file)
         with open(nextlaunch, 'w') as launch_file:
@@ -342,7 +380,26 @@ def reactops(message):
                 chat.post(':back: Reverting to previous version.')
                 with tarfile.open(base_dir +
                                   '/.cache/troupe-backup.tar') as tar:
-                    tar.extractall(path=base_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path=base_dir)
                 chat.post('relaunch {}'.format(device_name))
 
 
